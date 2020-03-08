2cf4ec6ae00a02d39a6e9aed217654bf655e481e
R00GER <roni.friman@sonit.fi>
2020-03-08 21:06:53 +0200

    Prevent submitting if email is empty

--- js/data.js
+++ js/data.js
@@ -1,17 +1,24 @@
 const email = document.querySelector("#email");
+
 let success = null;
 
 document.getElementById("submit").addEventListener("click", function() {
-    let xhr = new XMLHttpRequest();
-    xhr.open("GET", "updateDB.php?email=" + email.value, true);
-    xhr.send();
+    if (email.value === "") {
+        email.classList.add("redborder");
+        return
+    } else {
+        email.classList.remove("redborder");
+        let xhr = new XMLHttpRequest();
+        xhr.open("GET", "updateDB.php?email=" + email.value, true);
+        xhr.send();
 
-    xhr.onload = function() {
-        if (xhr.status != 200) {
-            success = false;
-        } else {
-            success = true;
+        xhr.onload = function() {
+            if (xhr.status != 200) {
+                success = false;
+            } else {
+                success = true;
+            }
+            openNewsletter(success);
         }
-        openNewsletter(success);
-    };
+    }
 });
